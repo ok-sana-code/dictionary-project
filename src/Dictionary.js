51412d026cc53a6ae3315140982b9edc4f978e1b
ok-sana-code <108321942+ok-sana-code@users.noreply.github.com>
2022-08-04 11:20:43 +0300

    Added API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,25 +1,35 @@
-import React, {useState} from "react";
-import "./Dictionary.css"
+import React, { useState } from "react";
+import "./Dictionary.css";
+import axios from "axios";
 
-export default function Dictionary(){
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
 
-let[keyword,setKeyword] = useState("")
+  function HandleResponse(response) {
+    console.log(response.data[0]);
+  }
 
-function search(event){
+  function search(event) {
     event.preventDefault();
-    alert(`Searchind for ${keyword} definition`);
-}
+    // Dokumentation: https://dictionaryapi.dev/
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(HandleResponse);
+  }
 
-function HandleChangeValue(response){
-    setKeyword(response.target.value)
-}
+  function HandleChangeValue(response) {
+    setKeyword(response.target.value);
+  }
 
-
-    return <div className="Dictionary">
-        <form onSubmit={search}>
-            <input type="rearch" placeholder="Type a word.." autoFocus={true} onChange={HandleChangeValue}>
-            </input>
-        </form>
-        
-       </div>;
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input
+          type="rearch"
+          placeholder="Type a word.."
+          autoFocus={true}
+          onChange={HandleChangeValue}
+        ></input>
+      </form>
+    </div>
+  );
 }
