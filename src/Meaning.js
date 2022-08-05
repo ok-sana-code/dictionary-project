d8ffef142a7910256c64478b055a0937ab38d6fe
ok-sana-code <108321942+ok-sana-code@users.noreply.github.com>
2022-08-05 11:20:43 +0300

    Added  Components (Example and Synonyms)

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,18 +1,24 @@
 import React from "react";
 import "./Meaning.css";
+import Synonyms from "./Synonyms"
+import Example from "./Example"
+
 export default function Meaning(props) {
-  // console.log(props.meaning);
+  
   return (
     <div className="Meaning shadow mt-4">
       <h3>{props.meaning.partOfSpeech}</h3>
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <p>
+            
+              <strong>Definition: </strong>
               {definition.definition}
               <br />
-              <em>{definition.example}</em>
-            </p>
+              <Example example={definition.example} />        
+              <br />
+              <Synonyms synonyms={definition.synonyms} />
+          
           </div>
         );
       })}
